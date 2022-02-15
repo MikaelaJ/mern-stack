8f161e966347d5c5f4cd22be8db2d96d2106e953
Mikaela Johansson <mikaela.johansson@pensionera.se>
2022-02-15 13:52:17 +0100

    feat: Add error handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,13 +2,18 @@
 // @route   GET /api/goals
 // @access  Private
 const getGoals = (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+    res.status(200)
 }
 
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
 const setGoal = (req, res) => {
+    if(req.body.text) {
+        //res.status(400).json({message: "Please add a text field"})
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
     res.status(200).json({message: 'Set goal'})
 }
 
