f1004ad46c3b31061f85a03595f0ce4aa65d4ab3
Mikaela Johansson <mikaela.johansson@pensionera.se>
2022-02-24 22:35:56 +0100

    feat: Add jwt, login and register user details

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,28 +12,81 @@ const registerUser = asyncHandler(async (req, res) => {
     // destruct the respone
     const { name, email, password } = req.body
 
-if(!name || !email || !password) {
-    res.status(400)
-    throw new Error('Please fill in all fields')
-}
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please fill in all fields')
+    }
 
-    res.json({message: 'Register User'})
+    // Check if user exists
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User Already exists')
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create user, pass and object with the field that we want. Make suse we use the hashed password
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    // If the user was created, set sttus to 201 which means ok and that something was created
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: 'Login User'})
+    const { email, password } = req.body
+
+    // Check for user email
+    const user = await User.findOne({ email })
+
+    // Check password, compare the hashed password 
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
 })
 
 // @desc    Get user user
 // @route   GET /api/users/me
 // @access  Public
 const getMe = asyncHandler(async (req, res) => {
-    res.json({message: 'User data information'})
+    res.json({ message: 'User data information' })
 })
 
+// Create a function that generates a token
+// that take in the user ID as the payload
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 module.exports = {
     registerUser,
