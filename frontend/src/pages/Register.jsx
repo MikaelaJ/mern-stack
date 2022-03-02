969b30d6666977c4bcc0742f8a81c39ca6bfa842
Mikaela Johansson <mikaela.johansson@pensionera.se>
2022-03-02 22:14:55 +0100

    feat: Add form to register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { FaUser } from 'react-icons/fa'
 
 function Register() {
-  return (
-    <div>Register</div>
-  )
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    })
+
+    const { name, email, password, password2 } = formData
+
+    const onChange = () => { }
+
+    return (
+        <>
+            <section className='heading'>
+                <h1>
+                    <FaUser />
+                </h1>
+                <p>Please create an accont</p>
+            </section>
+            <section className="form">
+                <form>
+                    <div className="form-group">
+
+                    </div>
+                    <input type="text" className="form-control" id="name" name="name" value={name} placeholder="Enter your name" onchange={onChange} />
+                </form>
+
+            </section>
+        </>
+    )
 }
 
 export default Register
