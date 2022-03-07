4c635bde4d4fd8aa9fa0c799cb71660426e70c15
Mikaela Johansson <mikaela.johansson@pensionera.se>
2022-03-07 17:09:36 +0100

    feat: Add authService

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,19 +1,30 @@
 // Login and Register
-import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import authService from './authService'
 
 // GEt user from localstorage
 const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
-    user: user ? user: null,
-    isError: false, 
+    user: user ? user : null,
+    isError: false,
     isSuccess: false,
     isLOading: false,
     message: "",
 }
 
+// Register user
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
+    try {
+        return await authService.register(user)
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
 export const authSlice = createSlice({
-    name: "auth", 
+    name: "auth",
     initialState,
     reducers: {
         reset: (state) => {
@@ -23,8 +34,19 @@ export const authSlice = createSlice({
             state.message = ""
         }
     },
-    extraReducers: () => {}
+    extraReducers: (builder) => {
+        builder
+        .addCase(register.pending, (state) => {
+            state.isLoading = true
+        })
+        .addCase(register.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.user = action.payload
+        })
+        .addCase()
+    }
 })
 
-export const {reset} = authSlice.actions
+export const { reset } = authSlice.actions
 export default authSlice.reducer
