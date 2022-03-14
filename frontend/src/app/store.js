31220ab6d2cf3f0341c3a30add2eb7fe300d4964
Mikaela Johansson <mikaela.johansson@pensionera.se>
2022-03-14 17:46:33 +0100

    feat: Add goalReducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from "../features/auth/authSlice"
-/* import counterReducer from '../features/counter/counterSlice'; */
+import goalReducer from "../features/goals/goalSlice"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    goal: goalReducer,
   },
 });
---
+++ frontend/src/features/goals/goalService.js
@@ -0,0 +1,22 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+// CREATESLICE: A function that accepts an initial state, an object of reducer functions, and a "slice name",
+// and automatically generates action creators and action types that correspond to the reducers and state.
+
+const initialState = {
+    goals: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    messge: '',
+}
+
+export const goalSlice = createSlice({
+    name: 'goal',
+    initialState, 
+    reducers: {
+        reset: (state) => initialState
+    }
+})
+
+export const {reset} = goalSlice.actions
+export default goalSlice.reducer
