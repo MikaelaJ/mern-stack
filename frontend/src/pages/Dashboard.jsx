2893159550aefefd4a1e39793d3f81c0f274920e
Mikaela Johansson <mikaela.johansson@pensionera.se>
2022-03-15 17:33:21 +0100

    feat: Add form for submitting Goals

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector} from 'react-redux'
+import GoalForm from '../components/GoalForm'
+
 
 function Dashboard() {
   const navigate = useNavigate()
   const {user} = useSelector((state) => state.auth)
 
   useEffect(() => {
+    // if no user, navigate to /login
     if (!user) {
       navigate('/login')
     }
-  }, [user, navigate]);
+  }, [user, navigate]); // dependency array
   return (
-    <div>Dashboard</div>
+    <>
+    <section className='heading'>
+      <h1>Welcome {user && user.name}</h1>
+      <p>Goals dashboard</p>
+    </section>
+    <GoalForm/>
+    </>
   )
 }
 
