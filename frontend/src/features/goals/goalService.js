7de3eca532a6641e7e6057571224cf0c0489aba1
Mikaela Johansson <mikaela.johansson@pensionera.se>
2022-03-23 18:49:48 +0100

    feat: Add more goal functionality

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -23,9 +23,22 @@ const getGoal = async (goalData, token) => {
     const response = await axios.get(API_URL, config)
     return response.data
 }
+
+// Delete user goals
+const deleteGoal = async (goalId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const response = await axios.delete(API_URL + goalId, config)
+    return response.data
+}
+
 const goalService = {
     createGoal,
-    getGoal
+    getGoal,
+    deleteGoal
 }
 
 export default goalService
